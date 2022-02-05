d17ffab0aaf5f2d2925192b98a6d8aa8a257f8fd
K McClelland <krmcclelland@users.noreply.github.com>
2022-02-05 17:13:52 -0600

    Add files via upload

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -10,9 +10,18 @@ let map = L.map("mapid", {
   zoom: 4
 });
   
+  //  Add a marker to the map for Los Angeles, California.
+  L.circleMarker([34.0522, -118.2437], {
+    radius: 300,
+    color: "black",
+    fillColor: '#ffffa1' 
+  }).addTo(map);
+
+  // let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
   // Add a tile layer (the background map image) to our map
   // We use the addTo method to add objects to our map
-  let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data (c); <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
